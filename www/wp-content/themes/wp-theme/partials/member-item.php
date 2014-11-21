1a423c3e822f7f9eef8ddbbc6aeb78a19e49f7c7
Charlie Rosenbury <gmail@rosenbury.com>
2014-11-20 21:11:08 -0600

    temporarily remove member link

--- www/wp-content/themes/wp-theme/partials/member-item.php
+++ www/wp-content/themes/wp-theme/partials/member-item.php
@@ -1,8 +1,6 @@
 <li class="person-item">
-    <a href="<?php echo $link; ?>">
-        <img src="<?php echo $user_image; ?>" alt="<?php echo $name; ?>" />
-    </a>
-    <h3><a href="<?php echo $link; ?>"><?php echo $name; ?></a></h3>
+    <img src="<?php echo $user_image; ?>" alt="<?php echo $name; ?>" />
+    <h3><?php echo $name; ?></h3>
     <h4><?php echo $subtitle ?></h4>
     <a href="mailto:<?php echo $email; ?>" class="email">Email</a>
 </li>
