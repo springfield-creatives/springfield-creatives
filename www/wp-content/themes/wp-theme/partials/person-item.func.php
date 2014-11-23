6dfaad3b36547c5460c73fbcd786e78753da207c
Charlie Rosenbury <gmail@rosenbury.com>
2014-11-23 17:50:19 -0600

    hide members' emails from non-logged in visitors

--- www/wp-content/themes/wp-theme/partials/person-item.func.php
+++ www/wp-content/themes/wp-theme/partials/person-item.func.php
@@ -61,7 +61,13 @@ function render_person_item($user, $subtitle = false){
 		</a>
 		<h3><a href="<?php echo $link ?>"><?php echo $name ?></a></h3>
 		<h4><?php echo $subtitle ?></h4>
-		<a href="mailto:<?php echo $email ?>" class="email">Email</a>
+		<?php
+		if(is_user_logged_in()){
+			?>
+			<a href="mailto:<?php echo $email ?>" class="email">Email</a>
+			<?php
+		}
+		?>
 	</li>
 	<?php
 }
