9b5476679ed3be8b1b44a18b2a87daa230721634
Charlie Rosenbury <gmail@rosenbury.com>
2014-11-23 09:16:37 -0600

    fix author page redirect

--- www/wp-content/themes/wp-theme/author.php
+++ www/wp-content/themes/wp-theme/author.php
@@ -1,12 +1,12 @@
 <?php
 
-the_post();
+$user_id = get_query_var('author');
 
 // temporarily redirect to their configged URL
-$url = get_the_author_meta( 'user_url' );
+$url = get_the_author_meta( 'user_url', $user_id );
 
 if(empty($url))
-	$url = 'https://www.google.com/#q=' . get_the_author_meta( 'user_firstname' ) . ' ' . get_the_author_meta( 'user_lastname' );
+	$url = 'https://www.google.com/#q=' . get_the_author_meta( 'user_firstname', $user_id ) . ' ' . get_the_author_meta( 'user_lastname', $user_id );
 
 header('Location: ' . $url,true,301);
 exit;
