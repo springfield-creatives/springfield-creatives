24890bf7ad17c9177dc3096906f7dcc243453408
Charlie Rosenbury <gmail@rosenbury.com>
2015-03-05 16:07:59 -0600

    enable preview mode for author profile

--- wp-content/themes/wp-theme/author.php
+++ wp-content/themes/wp-theme/author.php
@@ -1,5 +1,19 @@
 <?php
 
+if(!isset($_GET['preview'])){
+	$user_id = get_query_var('author');
+
+	// temporarily redirect to their configged URL
+	$url = get_the_author_meta( 'user_url', $user_id );
+
+	if(empty($url))
+		$url = 'https://www.google.com/#q=' . get_the_author_meta( 'user_firstname', $user_id ) . ' ' . get_the_author_meta( 'user_lastname', $user_id );
+
+	header('Location: ' . $url,true,301);
+	exit;
+}
+
+
 // get user info
 $user_id = get_query_var('author');
 $user = get_user_by('id', $user_id);
