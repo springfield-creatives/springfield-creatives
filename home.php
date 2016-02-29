511894e8237cd7f7a9ef7f569cac89312ac75d4d
Chad Spencer <CSpencer@paylocity.com>
2016-02-29 15:20:59 -0600

    blueprint styles

--- home.php
+++ home.php
@@ -3,9 +3,20 @@
   include 'includes/header.php';
 ?>
 
-<section class="hero" style="background-image: url(media/images/hero.jpg);">
+<section class="hero overlay" style="background-image: url(media/images/hero.jpg);">
   <article>
     <img src="media/images/logo-inverse.svg" width="500" />
+  </article>
+</section>
+<section class="alt narrow">
+  <article>
+    <div class="grid">
+      <div class="unit-3-5 unit-1-1-sm unit-center">
+        <h3>Tuesday, January 19th:  SGFC Monthy Meetup</h3>
+      </div>
+      <div class="unit-2-5 unit-1-1-sm text-right unit-center">
+        <p class="clean"><a href="">More Information ›</a></p>
+      </div>
   </article>
 </section>
 
