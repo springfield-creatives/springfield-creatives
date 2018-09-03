e977c7377f06596df1ff7a51cbeb83f0160ed9fa
Tom Legens <tlegens@Thomass-MacBook-Pro.local>
2018-09-03 16:43:32 -0500

    post a job page edits

--- wp-content/themes/sgfc2017/functions/custom_sidebars.php
+++ wp-content/themes/sgfc2017/functions/custom_sidebars.php
@@ -10,3 +10,12 @@ register_sidebar(array(
 	'after_widget'  => '</div>',
 	'before_title'  => '<h3>',
 	'after_title'   => '</h3>' ));
+
+register_sidebar(array(
+    'name'          => 'Job Posting',
+    'id'            => 'job-posting',
+    'description'   => '',
+    'before_widget' => '<div class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>' ));
--- wp-content/themes/sgfc2017/template-post-job.php
+++ wp-content/themes/sgfc2017/template-post-job.php
@@ -7,7 +7,7 @@ get_header();
 the_post();
 
 ?>
-<section>
+<section class="job-post-container">
   <article>
     <h2 class="margin text-center">Post A Job</h2>
     <div class="grid grid-center margin-double">
@@ -19,13 +19,25 @@ the_post();
     <?php
     if(!is_user_logged_in()):
     	?>
-	    <div class="woocommerce-message"><p>Please sign in or create an account before posting your job. If you are not logged in, you won't be able to edit it later.</p>
-    	<?php echo do_shortcode('[woocommerce_social_login_buttons]'); ?></div>
-	    <?php
-            else: the_content();
-    endif;
-    ?>
-    <?php // the_content() ?>
+	    <div class="woocommerce-message text-center">
+            <p>Please sign in or create an account before posting your job. If you are not logged in, you won't be able to edit it later.</p>
+            <div class="grid account-row">
+                <div class="login unit-2-4 text-center">
+                    <h3>Log In</h3>
+                    <p>Log in to your Springfield Creatives account</p>
+                    <?php dynamic_sidebar( 'job-posting' ); ?>
+                </div>
+                <div class="account unit-2-4 text-center">
+                    <h3>Create Account</h3>
+                    <p>Create a Springfield Creatives account</p>
+                </div>
+            </div>
+            <h3 class="text-center">Social Login</h3>
+            <?php echo do_shortcode('[woocommerce_social_login_buttons]'); ?>
+        </div>
+
+	    <?php endif; ?>
+        <?php the_content() ?>
 
   </article>
 </section>
