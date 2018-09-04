6f5822ead13230287ea7634e9b2c9babaec8f318
Tom Legens <tlegens@Thomass-MacBook-Pro.local>
2018-09-04 09:22:12 -0500

    add placeholders to log in form

--- wp-content/themes/sgfc2017/functions.php
+++ wp-content/themes/sgfc2017/functions.php
@@ -111,6 +111,23 @@ include_once('functions/site/profile_editor_frontend.php');
 include_once('functions/site/gravity_forms.php');
 include_once('functions/site/member-card-funcs.php');
 
+// WOOCOMMERCE
+
+// LOG IN FORM PLACEHOLDERS
+add_filter( 'gform_userregistration_login_form', 'change_form', 10, 1 );
+function change_form( $form ) {
+    $fields = $form['fields'];
+    foreach ( $fields as &$field ) {
+        if ( $field->label == 'Username' ) {
+            $field->placeholder = 'Email Address';
+        }
+        if ( $field->label == 'Password' ) {
+            $field->placeholder = 'Password';
+        }
+    }
+    return $form;
+}
+
 // // Author slug rewrite
 include_once('functions/site/author-slug-rewrite.php');
 
